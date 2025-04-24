95959dbffab903371cc89c07f925bc11665ba621
Kester <kestertan040@gmail.com>
2025-04-23 20:58:48 -0400

    using model directory to load instead

--- fine-tune/train.py
+++ fine-tune/train.py
@@ -3,8 +3,9 @@ import torch
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 
 config = AutoConfig.from_pretrained("../../gorilla-7b-hf-v0/config.json")
-model = AutoModelForCausalLM.from_config(config)
-model.load_state_dict(torch.load("../../gorilla-7b-hf-v0/"))
+model_directory = "../../gorilla-7b-hf-v0/"
+tokenizer = AutoTokenizer.from_pretrained(model_directory)
+model = AutoModelForCausalLM.from_pretrained(model_directory)
 
 model = prepare_model_for_kbit_training(model)
 lora_config = LoraConfig(
