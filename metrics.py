350cff247ad5ed4fc43ddbcb7c022bc6dc811e22
Jocelyn H <joceyjhao@jocelyns-air-2.wifi.local.cmu.edu>
2025-04-26 20:01:19 -0400

    hi

--- metrics.py
+++ metrics.py
@@ -1,37 +1,73 @@
 import json
+import pandas as pd
 from tqdm import tqdm
 
 nestful = "testing/nestful_data.json"
 result = "testing/results_last_360.json"
 
-with open(nestful, "r") as f:
-    nestful_data = json.load(f)
+df = pd.read_parquet("hf://datasets/ibm-research/nestful/data/train-00000-of-00001.parquet")
+
+nestful_data = df[-361:]
+
+print(df[-361:]['output'].iloc[0])
+
+s = df[-361:]['output'].iloc[0]
+
+json_array = json.loads(s)
+
+for d in json_array:
+    print(d)
+
+
+
+
+# with open(nestful, "r") as f:
+#     nestful_data = json.load(f)
 
 with open(result, "r") as f:
     result_data = json.load(f)
 
-for d in nestful_data[-360:][:10]:
 
-    # sample_id, input, output, tools, gold_answer
-    print(d["input"])
-    # print(d["input"])
-    output = d["output"]
-    functions = [output[i]["name"] for i in range(len(output))]
-    # print(functions)
-    # for function in output:
 
-print("\n")
+# for d in nestful_data[-360:][:10]:
 
-for d in result_data[:10]:
-    question = d["questions"]
+#     # sample_id, input, output, tools, gold_answer
+#     # print(d["input"])
+#     # print(d["input"])
+#     output = d['output']
+#     # functions = [output[i]["name"] for i in range(len(output))]
+#     # print(functions)
+#     # for function in output:
+
+for i in range(5):
+
+    question = result_data[i]["questions"]
+    response = result_data[i]["response"]
+
     index = question.find('Use this API documentation')
     input = question.strip()[9:index - 4]
+    if not (nestful_data['input'].iloc[i] == input):
+        print("ummmm")
+        continue
 
-    response = d["response"]
+
+    real_output = json.loads(nestful_data['output'].iloc[i])
+    functions = [real_output[i]["name"] for i in range(len(real_output))]
+    pred_output = "" # FILL THIS IN LATER????
+    correct_count = 0
+    for f in functions:
+        if f in pred_output:
+            correct_count += 0
+
+    
+
+
+
+    index = response.find("<<<code>>>")
+
 
     # print(response)
     # index = response.find("import")
-    print(input)
     # print(index)
     # print("\n")
     # .split('\\n')
