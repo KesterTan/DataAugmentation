94a501b5902d6a30a7239c269c17f969522d3d41
Kester <kestertan040@gmail.com>
2025-04-27 01:27:20 -0400

    test max length

--- open-functions/fine-tune.py
+++ open-functions/fine-tune.py
@@ -12,11 +12,11 @@ model = prepare_model_for_kbit_training(model)
 
 lora_config = LoraConfig(
     r=8,
-    lora_alpha=32,
+    lora_alpha=16,
     lora_dropout=0.1,
     bias="none",
     task_type="CAUSAL_LM",
-    target_modules=["q_proj", "v_proj", "k_proj", "o_proj", "up_proj", "down_proj", "gate_proj"]
+    target_modules=["q_proj", "v_proj", "k_proj", "o_proj"]
 )
 
 model = get_peft_model(model, lora_config)
