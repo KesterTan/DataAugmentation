3cb8203ce0a560a676424ec166083c1f84fb13e1
Joy Song <joys@andrew.cmu.edu>
2025-05-01 15:47:51 -0400

    clean splitdata.py

--- splitdata.py
+++ splitdata.py
@@ -1,20 +1,14 @@
 import json
 
-# Path to the original JSON file
 input_path = "display-all-training-data.json"
-
-# Load the JSON data from the file
 with open(input_path, "r") as f:
     data = json.load(f)
 
-# Extract the last 360 entries
+# Extract the last 360 entries (or first 1500)
 rest = data[-361:]
 # rest = data[:-360]
 
-# Path to the output JSON file
 output_path = "split_original_last360.json"
-
-# Save the last 360 entries to a new file
 with open(output_path, "w") as f:
     json.dump(rest, f, indent=2)
 
